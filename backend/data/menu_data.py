27c397e2ebfb87debfd1cb8c971767508388e3c3
Netrololo <danilgusenkov2001@gmail.com>
2026-06-19 09:09:02 +0700

    Add files via upload

---
+++ backend/data/menu_data.py
@@ -0,0 +1,34 @@
+DEFAULT_MENU = [
+  {
+    "id": 1,
+    "name": "Авокадо",
+    "description": "Это авокадо",
+    "price": 2050,
+    "category": "Закуски",
+    "image": "avocado.jpg"
+  },
+  {
+    "id": 2,
+    "name": "Пиво",
+    "description": "Это пиво",
+    "price": 250,
+    "category": "Напитки",
+    "image": "beer.jpg"
+  },
+  {
+    "id": 3,
+    "name": "Мороженое",
+    "description": "Это мороженое",
+    "price": 250,
+    "category": "Десерты",
+    "image": "icecream.jpg"
+  },
+  {
+    "id": 4,
+    "name": "Стейк",
+    "description": "Это стейк (пережаренный в хлам)",
+    "price": 250,
+    "category": "Основные блюда",
+    "image": "steak.jpg"
+  }
+]
